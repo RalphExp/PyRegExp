b311143e1a074ebc996b30578459f67af7be3783
ralph <ralphxwz@gmail.com>
2023-04-10 14:17:12 +0800

    update

--- re2.py
+++ re2.py
@@ -1,7 +1,46 @@
 #!/usr/bin/python3
 
-# This file shows how to use NFA to construct a regular expression engine.
-# for fun only :)
+""" This module provides a simplified version of regular expression
+matching operations of python're library. It supports both 8-bit and 
+Unicode strings; both the pattern and the strings being processed 
+can contain null bytes and characters outside the US ASCII range.
+
+The special characters are:
+    "."      Matches any character except a newline.
+    "*"      Matches 0 or more (greedy) repetitions of the preceding RE.
+                Greedy means that it will match as many repetitions as possible.
+    "+"      Matches 1 or more (greedy) repetitions of the preceding RE.
+    "?"      Matches 0 or 1 (greedy) of the preceding RE.
+    *?,+?,?? Non-greedy versions of the previous three special characters.
+    "|"      A|B, creates an RE that will match either A or B.
+    (...)    Matches the RE inside the parentheses.
+    "\\"     Either escapes special characters or signals a special sequence.
+
+    The special sequences consist of "\\" and a character from the list
+    below.  If the ordinary character is not on the list, then the
+    resulting RE will match the second character.
+        \d       Matches any decimal digit; equivalent to the set [0-9] in
+                 bytes patterns or string patterns with the ASCII flag.
+                 In string patterns without the ASCII flag, it will match the whole
+                 range of Unicode digits.
+        \D       Matches any non-digit character; equivalent to [^\d].
+        \s       Matches any whitespace character; equivalent to [ \t\n\r\f\v] in
+                 bytes patterns or string patterns with the ASCII flag.
+                 In string patterns without the ASCII flag, it will match the whole
+                 range of Unicode whitespace characters.
+        \S       Matches any non-whitespace character; equivalent to [^\s].
+        \w       Matches any alphanumeric character; equivalent to [a-zA-Z0-9_]
+                 in bytes patterns or string patterns with the ASCII flag.
+                 In string patterns without the ASCII flag, it will match the
+                 range of Unicode alphanumeric characters (letters plus digits
+                 plus underscore).
+                 With LOCALE, it will match the set [0-9_] plus characters defined
+                 as letters for the current locale.
+        \W       Matches the complement of \w.
+        \u       Matches unicode characters.
+
+For fun only :)
+"""
 
 from __future__ import annotations
 from collections import OrderedDict
